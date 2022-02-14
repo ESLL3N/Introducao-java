2292c4bed0a03844ed5aa17d29e550031b7d7e14
ESLL3N <esllenescolar@gmail.com>
2022-02-14 17:51:32 +0000

    introdução

---
+++ Ola.java
@@ -0,0 +1,10 @@
+//toda classe tem que ter o mesmo nome do arquivo;
+//o metodo "main" é o metodo principal;
+//para copilar a aplicação use 'javac "nome do arquivo"' e para executar use 'java "nome do arquivo"';
+
+
+public class Ola {
+    public static void main(String[] args) {
+        System.out.println("Olá Mundo!!");
+    }
+}
