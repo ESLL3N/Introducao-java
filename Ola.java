5df7f054cfcf5e9f264ebfdac01cc9ac0cc5150b
ESLL3N <esllenescolar@gmail.com>
2022-02-16 17:51:48 +0000

    soma e tipos de dados

--- Ola.java
+++ Ola.java
@@ -4,7 +4,11 @@
 
 
 public class Ola {
+    public static int soma(int a, int b) {
+        return a + b;
+    }
     public static void main(String[] args) {
         System.out.println("Olá Mundo!!");
+        System.out.println(soma(4,6));
     }
 }
