d861f3a9813da66772dc8f5ca49b2fb5a651f98d
ESLL3N <esllenescolar@gmail.com>
2022-02-21 17:55:06 +0000

    conceitos

--- Ola.java
+++ Ola.java
@@ -1,7 +1,9 @@
 //toda classe tem que ter o mesmo nome do arquivo;
 //o metodo "main" é o metodo principal;
 //para copilar a aplicação use 'javac "nome do arquivo"' e para executar use 'java "nome do arquivo"';
-
+// != <- diferente // == <- igual 
+// === <- compara tipo e valor da variável
+// atribuidor composto // int a = 10; --> a += 20; é a mesma coisa  que 'a = a + 20;'
 
 public class Ola {
     public static int soma(int a, int b) {
@@ -10,5 +12,5 @@ public class Ola {
     public static void main(String[] args) {
         System.out.println("Olá Mundo!!");
         System.out.println(soma(4,6));
-    }
+    } 
 }
