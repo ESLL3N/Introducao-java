5568203b84e1cb826f0d83f6cbe17784c59a8d1b
ESLL3N <esllenescolar@gmail.com>
2022-02-23 17:55:08 +0000

    tipos primitivos

--- Ola.java
+++ Ola.java
@@ -4,6 +4,9 @@
 // != <- diferente // == <- igual 
 // === <- compara tipo e valor da variável
 // atribuidor composto // int a = 10; --> a += 20; é a mesma coisa  que 'a = a + 20;'
+// o 'char' vai me permitir que eu tenha apenas um caracter;
+// boolean é o verdadeiro ou falso 'true ou false' sempre minusculo pois ele é case sensitive;
+
 
 public class Ola {
     public static int soma(int a, int b) {
@@ -12,5 +15,19 @@ public class Ola {
     public static void main(String[] args) {
         System.out.println("Olá Mundo!!");
         System.out.println(soma(4,6));
+
+        boolean bol = false;
+        char ch = 'a';
+        byte intB = 2;
+        short intS = 200;
+        int intN = 1000;
+        long intL = 2000;
+        float fNum = 2.5f;
+        double dNum = 2.5;
+    
+
+        System.out.println(Float.MIN_VALUE);
+        System.out.println(Float.MAX_VALUE);
+
     } 
 }
